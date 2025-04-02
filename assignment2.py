ad7f5eaff440aa681b3ebe5b29ff787bd0ede52f
Yu-Fan Yang <yyang334@myseneca.ca>
2025-04-02 15:50:15 -0400

    add create and delete function

--- assignment2.py
+++ assignment2.py
@@ -13,11 +13,24 @@ import subprocess
 
 #author: YuFan
 def create_user(username):
-    pass
+    print("user created here")
+
+    try:
+        # Using sudo to run the useradd command
+        subprocess.run(["sudo", "useradd", username], check=True)
+        print(f'User {username} added successfully.')
+    except subprocess.CalledProcessError as e:
+        print(f'Failed to add user {username}: {e}')
 
 #author: YuFan
-def delete_user(username):
-    pass
+def delete_user(username):    
+    print("user deleted here")
+    try:
+        # Delete the user using userdel
+        subprocess.check_call(["sudo", "userdel", username])
+        print(f'User {username} deleted successfully.')
+    except subprocess.CalledProcessError as e:
+        print(f'Failed to delete user {username}: {e}')
 
 #author: YuFan
 def list_human_users():
@@ -50,54 +63,54 @@ def show_help():
     pass
 
 #Test argparse module
-parser = argparse.ArgumentParser()
-parser.add_argument("echo123", help="echo the string you use here", type=int, choices=[1, 2, 3])
-args = parser.parse_args()
-print(args.echo123)
-
-
-# parser = argparse.ArgumentParser(description="User Management Script for Linux Systems")
-# parser.add_argument(
-#     'action',
-#     choices=['create', 'delete', 'list', 'locked', 'size', 'current', 'loggedin', 'group', 'password'],
-#     help="Action to perform: create, delete, list, locked, size, current, loggedin, group, password"
-# )
-# parser.add_argument(
-#     '--username',
-#     type=str,
-#     help="Specify the username (required for actions 'create', 'delete', 'size', 'group', 'password')"
-# )
-# parser.add_argument(
-#     '--group',
-#     type=str,
-#     help="Specify the group (required for the 'group' action)"
-# )
-
+# parser = argparse.ArgumentParser()
+# parser.add_argument("echo123", help="echo the string you use here", type=int, choices=[1, 2, 3])
 # args = parser.parse_args()
+# print(args.echo123)
 
-# if len(vars(args)) == 0:
-#     show_help()
-# else:
-#     try:
-#         validate_args(args)
-#         if args.action == 'create':
-#             create_user(args.username)
-#         elif args.action == 'delete':
-#             delete_user(args.username)
-#         elif args.action == 'list':
-#             list_human_users()
-#         elif args.action == 'locked':
-#             find_locked_or_disabled_accounts()
-#         elif args.action == 'size':
-#             check_user_directory_size(args.username)
-#         elif args.action == 'current':
-#             check_current_user()
-#         elif args.action == 'loggedin':
-#             check_logged_in_users()
-#         elif args.action == 'group':
-#             change_user_group(args.username, args.group)
-#         elif args.action == 'password':
-#             change_user_password(args.username)
-#     except ValueError as e:
-#         print(f"Argument error: {e}")
-#         show_help()
+
+parser = argparse.ArgumentParser(description="User Management Script for Linux Systems")
+parser.add_argument(
+    'action',
+    choices=['create', 'delete', 'list', 'locked', 'size', 'current', 'loggedin', 'group', 'password'],
+    help="Action to perform: create, delete, list, locked, size, current, loggedin, group, password"
+)
+parser.add_argument(
+    '--username',
+    type=str,
+    help="Specify the username (required for actions 'create', 'delete', 'size', 'group', 'password')"
+)
+parser.add_argument(
+    '--group',
+    type=str,
+    help="Specify the group (required for the 'group' action)"
+)
+
+args = parser.parse_args()
+
+if len(vars(args)) == 0:
+    show_help()
+else:
+    try:
+        validate_args(args)
+        if args.action == 'create':
+            create_user(args.username)
+        elif args.action == 'delete':
+            delete_user(args.username)
+        elif args.action == 'list':
+            list_human_users()
+        elif args.action == 'locked':
+            find_locked_or_disabled_accounts()
+        elif args.action == 'size':
+            check_user_directory_size(args.username)
+        elif args.action == 'current':
+            check_current_user()
+        elif args.action == 'loggedin':
+            check_logged_in_users()
+        elif args.action == 'group':
+            change_user_group(args.username, args.group)
+        elif args.action == 'password':
+            change_user_password(args.username)
+    except ValueError as e:
+        print(f"Argument error: {e}")
+        show_help()
